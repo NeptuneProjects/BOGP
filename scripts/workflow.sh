e46df86c14eec21fdf8debe8648d202c8ab0f59c
NeptuneProjects <william.f.jenkins@gmail.com>
2022-12-12 11:55:25 -0800

    Update workflow script for e-2-e

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 # 
-# Runs configuration and production scripts. Sample usage:
-# bash ./Source/scripts/workflow.sh l 4 cuda
+# Description of workflow
+# Before running, ensure scripts 'configurations.py' and 'aggregate.py' 
+# are configured for the desired workflow (i.e., specify acquisition 
+# functions, parameters, etc.)
 
-SIM="$1"
-JOBS=$2
-DEVICE=$(echo "$3" | awk '{print toupper($0)}')
-OFFSET=${4:-0}
+SIM="r"
+JOBS=8
+DEVICE="cpu"
+OFFSET=0
 
-if [[ $SIM == "r" ]]
-    then
-        SIMDESC="range estimation"
-elif [[ $SIM == "l" ]]
-    then
-        SIMDESC="localization"
-fi
-python3 ./Source/scripts/configurations.py $SIM
-echo "Running $SIMDESC on $DEVICE with $JOBS jobs."
-if [[ $DEVICE == "CPU" ]]
-    then
-        find ./Data/Simulations/localization/queue/*.json \
-        | parallel -j$JOBS --progress \
-        'python3 ./Source/scripts/simulations.py '$SIM' {} --path ./Data/Simulations'
-elif [[ $DEVICE == "CUDA" ]]
-    then
-        find ./Data/Simulations/localization/queue/*.json \
-        | parallel -j$JOBS --progress \
-        'CUDA_VISIBLE_DEVICES="$(({%}-1+'$OFFSET'))" python3 ./Source/scripts/simulations.py '$SIM' {} --path ./Data/Simulations --device cuda'
-fi
-python3 ./Source/scripts/aggregate.py r l
-python3 ./Source/scripts/aggregate.py s l
-# For debugging:
-# find ./Data/Simulations/localization/queue/*.json | parallel -j1 --progress 'CUDA_VISIBLE_DEVICES=1 python3 ./Source/scripts/simulations.py l {} --path ./Data/Simulations --device cuda'
+zsh ./Source/scripts/sim_config.sh $SIM
+zsh ./Source/scripts/sim_run.sh $SIM $JOBS $DEVICE $OFFSET
+zsh ./Source/scripts/sim_agg_results.sh $SIM
