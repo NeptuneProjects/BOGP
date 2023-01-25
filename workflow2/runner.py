85c475edd28d85fdc8a4e76413bc87a2ebfab3a7
NeptuneProjects <william.f.jenkins@gmail.com>
2023-01-25 14:31:06 -0800

    New runner script

---
+++ workflow2/runner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def main(mode, strategies):
+    return
+
+
+
+
+
+if __name__ == "__main__":
+    problem = "range_estimation"
+    mode = "simulation"
+    
+    
+    strategies = ["grid", "random", "sobol"]
+
+    main(mode, strategies)
+
