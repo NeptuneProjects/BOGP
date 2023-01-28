c1423b0e41929d0250c1af659f43fc644e09fb3b
NeptuneProjects <william.f.jenkins@gmail.com>
2023-01-28 00:54:18 -0800

    change to lowercase

--- scripts/_config.py
+++ scripts/_config.py
@@ -2,7 +2,6 @@
 
 from datetime import datetime
 from pathlib import Path
-import tomli
 
 import tomli_w
 
@@ -13,88 +12,57 @@ SERIAL = datetime.now().strftime("serial_%Y%m%dT%H%M%S")
 config = {
     "range_estimation": {
         "simulation": {
-            "NUM_MC_RUNS": 100,
-            "NUM_RESTARTS": 20,
-            "NUM_SAMPLES": 512,
-            "NUM_TRIALS": 200,
-            "NUM_WARMUP": 10,
-            "Q": 5,
-            "ROOT": ROOT,
-            "SEED": SEED,
-            "SERIAL": SERIAL
+            "num_mc_runs": 100,
+            "num_restarts": 20,
+            "num_samples": 512,
+            "num_trials": 200,
+            "num_warmup": 10,
+            "q": 5,
+            "root": ROOT,
+            "main_seed": SEED,
+            "serial": SERIAL
         },
         "experimental": {
-            "NUM_MC_RUNS": 100,
-            "NUM_RESTARTS": 20,
-            "NUM_SAMPLES": 512,
-            "NUM_TRIALS": 200,
-            "NUM_WARMUP": 10,
-            "Q": 5,
-            "ROOT": ROOT,
-            "SEED": SEED,
-            "SERIAL": SERIAL
+            "num_mc_runs": 1,
+            "num_restarts": 20,
+            "num_samples": 512,
+            "num_trials": 200,
+            "num_warmup": 10,
+            "q": 5,
+            "root": ROOT,
+            "main_seed": SEED,
+            "serial": SERIAL
         },
     },
     "localization": {
         "simulation": {
-            "NUM_MC_RUNS": 100,
-            "NUM_RESTARTS": 40,
-            "NUM_SAMPLES": 1024,
-            "NUM_TRIALS": 800,
-            "NUM_GRID_TRIALS": [20, 40],
-            "NUM_WARMUP": 20,
-            "Q": 5,
-            "ROOT": ROOT,
-            "SEED": SEED,
-            "SERIAL": SERIAL
+            "num_mc_runs": 100,
+            "num_restarts": 40,
+            "num_samples": 1024,
+            "num_trials": 800,
+            "num_grid_trials": [20, 40],
+            "num_warmup": 50,
+            "q": 5,
+            "root": ROOT,
+            "main_seed": SEED,
+            "serial": SERIAL
         },
         "experimental": {
-            "NUM_MC_RUNS": 100,
-            "NUM_RESTARTS": 40,
-            "NUM_SAMPLES": 1024,
-            "NUM_TRIALS": 800,
-            "NUM_GRID_TRIALS": [20, 40],
-            "NUM_WARMUP": 20,
-            "Q": 5,
-            "ROOT": ROOT,
-            "SEED": SEED,
-            "SERIAL": SERIAL
+            "num_mc_runs": 1,
+            "num_restarts": 40,
+            "num_samples": 1024,
+            "num_trials": 800,
+            "num_grid_trials": [20, 40],
+            "num_warmup": 50,
+            "q": 5,
+            "root": ROOT,
+            "main_seed": SEED,
+            "serial": SERIAL
         }
     }
 }
-
-# config = {
-#     "range_estimation": {
-#         "NUM_MC_RUNS": 100,
-#         "NUM_RESTARTS": 20,
-#         "NUM_SAMPLES": 512,
-#         "NUM_TRIALS": 200,
-#         "NUM_WARMUP": 10,
-#         "Q": 5,
-#         "ROOT": ROOT,
-#         "SEED": SEED,
-#         "SERIAL": SERIAL
-#     },
-#     "localization": {
-#         "NUM_MC_RUNS": 100,
-#         "NUM_RESTARTS": 40,
-#         "NUM_SAMPLES": 1024,
-#         "NUM_TRIALS": 800,
-#         "NUM_GRID_TRIALS": [20, 40],
-#         "NUM_WARMUP": 20,
-#         "Q": 5,
-#         "ROOT": ROOT,
-#         "SEED": SEED,
-#         "SERIAL": SERIAL
-#     }
-# }
 
 if __name__ == "__main__":
     config_path = (Path.cwd() / "Source" / "scripts" / "config.toml").relative_to(Path.cwd())
     with open(config_path, "wb") as fp:
         tomli_w.dump(config, fp)
-    
-    with open(config_path, "rb") as fp:
-        config_ = tomli.load(fp)
-    
-        print(config_)
