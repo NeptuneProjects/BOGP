a355809dd7f81e24a42b2f5e9c6852796104ac9a
NeptuneProjects <william.f.jenkins@gmail.com>
2023-02-22 18:05:32 -0800

    Refactor & format

--- BOGP/figures.py
+++ BOGP/figures.py
@@ -59,7 +59,7 @@ def figure6():
 
 
 # def figure7():
-#     return experimental_localization()
+    # return experimental_localization()
 
 
 def adjust_subplotticklabels(ax, low=None, high=None):
@@ -68,6 +68,29 @@ def adjust_subplotticklabels(ax, low=None, high=None):
         ticklabels[low].set_va("bottom")
     if high is not None:
         ticklabels[high].set_va("top")
+
+
+def experimental_localization():
+    AMBSURF_PATH = (
+        ROOT
+        / "Data"
+        / "SWELLEX96"
+        / "VLA"
+        / "selected"
+        / "multifreq"
+        / "148.0-166.0-201.0-235.0-283.0-338.0-388.0"
+    )
+    # Load high-res MFP
+    timesteps, ranges, depths = load_mfp_results(AMBSURF_PATH)
+
+    # GPS Range
+    gps_fname = ROOT / "Data" / "SWELLEX96" / "VLA" / "selected" / "gps_range.csv"
+    df_gps = pd.read_csv(gps_fname, index_col=0)
+    
+    serial = "serial_full_depth"
+    fname = ROOT / "Data" / "localization" / "experimental" / serial / "results" / "collated.csv"
+    df = pd.read_csv(fname, index_col=0)
+    return plot_experimental_results(df, df_gps, timesteps, ranges, depths)
 
 
 def experimental_range_est():
@@ -91,6 +114,168 @@ def experimental_range_est():
     fname = ROOT / "Data" / "localization" / "experimental" / serial / "results" / "collated.csv"
     df = pd.read_csv(fname, index_col=0)
     return plot_experimental_results(df, df_gps, timesteps, ranges, depths)
+
+
+def format_error(error, est_timesteps):
+    error_plot = np.empty(350)
+    error_plot[:] = np.nan
+    error_plot[est_timesteps] = error
+    return error_plot
+
+
+def plot_ambiguity_surface(
+    B,
+    rvec,
+    zvec,
+    ax=None,
+    cmap="jet",
+    vmin=-10,
+    vmax=0,
+    interpolation="none",
+    marker="*",
+    color="w",
+    markersize=15,
+    markeredgewidth=1.5,
+    markeredgecolor="k",
+):
+
+    if ax is None:
+        ax = plt.gca()
+
+    src_z_ind, src_r_ind = np.unravel_index(np.argmax(B), (len(zvec), len(rvec)))
+
+    im = ax.imshow(
+        B,
+        aspect="auto",
+        extent=[min(rvec), max(rvec), min(zvec), max(zvec)],
+        origin="lower",
+        vmin=vmin,
+        vmax=vmax,
+        interpolation=interpolation,
+        cmap=cmap,
+    )
+    ax.plot(
+        rvec[src_r_ind],
+        zvec[src_z_ind],
+        marker=marker,
+        color=color,
+        markersize=markersize,
+        markeredgewidth=markeredgewidth,
+        markeredgecolor=markeredgecolor,
+    )
+    ax.invert_yaxis()
+    return ax, im
+
+
+def plot_environment():
+    zw, cw, _ = read_ssp(
+        ROOT / "Data" / "SWELLEX96" / "CTD" / "i9606.prn", 0, 3, header=None
+    )
+    zw = np.append(zw, 217)
+    cw = np.append(cw, cw[-1])
+    zb1 = np.array([np.NaN, 217, 240])
+    cb1 = np.array([np.NaN, 1572.37, 1593.0])
+    zb2 = np.array([np.NaN, 240, 1040])
+    cb2 = np.array([np.NaN, 1881, 3245])
+    z1 = np.concatenate([zw, zb1])
+    c1 = np.concatenate([cw, cb1])
+
+    upper_zlim = [240, 0]
+    lower_zlim = [1100, 240]
+    lower_clim = [1450, 1600]
+    upper_clim = [1800, 3400]
+
+    fig, axs = plt.subplots(
+        figsize=(4, 5),
+        nrows=2,
+        ncols=2,
+        gridspec_kw={"wspace": 0.05, "hspace": 0, "width_ratios": [0.67, 0.33]},
+    )
+
+    ax = axs[0, 0]
+    ax.plot(c1, z1)
+    ax.axhline(217, c="k", lw=1)
+    ax.fill_between(lower_clim, 0, 217, color="lightblue", alpha=0.15, linewidth=0)
+    ax.fill_between(lower_clim, 217, 240, color="yellow", alpha=0.15, linewidth=0)
+    ax.set_xlim(lower_clim)
+    ax.set_xlabel("Sound Speed [m/s]")
+    ax.xaxis.set_ticks_position("top")
+    ax.xaxis.set_label_position("top")
+    ax.invert_yaxis()
+    ax.set_ylim(upper_zlim)
+    ax.set_yticks([0, 50, 100, 150, 200, 217])
+    ax.set_ylabel("Depth [m]", y=0)
+    ax.spines.right.set_linestyle((0, (5, 10)))
+    ax.spines.right.set_linewidth(0.5)
+    ax.spines.bottom.set_visible(False)
+
+    ax = axs[0, 1]
+    ax.axhline(217, c="k", lw=1)
+    ax.fill_between(upper_clim, 0, 217, color="lightblue", alpha=0.15, linewidth=0)
+    ax.fill_between(upper_clim, 217, 240, color="yellow", alpha=0.15, linewidth=0)
+    props = dict(boxstyle="round", facecolor="white", alpha=0.9)
+    ax.annotate(
+        "Sediment layer\n$\\rho = 1.76\ \mathrm{g\ cm^{-3}}$\n$a=0.2\ \mathrm{dB \ km^{-1}\ Hz^{-1}}$",
+        xy=(2500, 235),
+        xycoords="data",
+        xytext=(800, 185),
+        textcoords="data",
+        bbox=props,
+        arrowprops=dict(arrowstyle="->"),
+    )
+    ax.set_xlim(upper_clim)
+    ax.xaxis.set_tick_params(top=True, bottom=False)
+    ax.set_xticklabels([])
+    ax.set_ylim(upper_zlim)
+    ax.set_yticks([])
+    ax.spines.left.set_linestyle((0, (5, 10)))
+    ax.spines.left.set_linewidth(0.5)
+    ax.spines.bottom.set_visible(False)
+
+    ax = axs[1, 0]
+    ax.axhline(242, c="k", lw=1)
+    ax.axhline(1040, c="k", lw=1)
+    ax.fill_between(lower_clim, 240, 1040, color="tan", alpha=0.15, linewidth=0)
+    ax.fill_between(lower_clim, 1040, 1100, color="gray", alpha=0.15, linewidth=0)
+    ax.text(
+        1460,
+        450,
+        "Mudrock layer\n$\\rho = 2.06\ \mathrm{g\ cm^{-3}}$\n$a=0.06\ \mathrm{dB \ km^{-1}\ Hz^{-1}}$",
+        bbox=props,
+        va="center",
+    )
+    ax.annotate(
+        "Bedrock halfspace\n$c = 5200\ \mathrm{m\ s^{-1}}}$\n$\\rho = 2.66\ \mathrm{g\ cm^{-3}}$\n$a=0.02\ \mathrm{dB \ km^{-1}\ Hz^{-1}}$",
+        xy=(1500, 1090),
+        xycoords="data",
+        xytext=(1460, 940),
+        textcoords="data",
+        bbox=props,
+        arrowprops=dict(arrowstyle="->"),
+    )
+    ax.set_xlim(lower_clim)
+    ax.set_xticklabels([])
+    ax.set_ylim(lower_zlim)
+    ax.set_yticks([240, 400, 600, 800, 1040])
+    ax.spines.right.set_linestyle((0, (5, 10)))
+    ax.spines.right.set_linewidth(0.5)
+    ax.spines.top.set_visible(False)
+
+    ax = axs[1, 1]
+    ax.plot(cb2, zb2)
+    ax.axhline(242, c="k", lw=1)
+    ax.axhline(1040, c="k", lw=1)
+    ax.fill_between(upper_clim, 240, 1040, color="tan", alpha=0.15, linewidth=0)
+    ax.fill_between(upper_clim, 1040, 1100, color="gray", alpha=0.15, linewidth=0)
+    ax.set_xlim(upper_clim)
+    ax.invert_yaxis()
+    ax.set_ylim(lower_zlim)
+    ax.set_yticks([])
+    ax.spines.left.set_linestyle((0, (5, 10)))
+    ax.spines.left.set_linewidth(0.5)
+    ax.spines.top.set_visible(False)
+
+    return fig
 
 
 def plot_experimental_results(df, df_gps, timesteps, ranges, depths):
@@ -315,168 +500,6 @@ def plot_experimental_results(df, df_gps, timesteps, ranges, depths):
     return fig
 
 
-def format_error(error, est_timesteps):
-    error_plot = np.empty(350)
-    error_plot[:] = np.nan
-    error_plot[est_timesteps] = error
-    return error_plot
-
-
-def plot_ambiguity_surface(
-    B,
-    rvec,
-    zvec,
-    ax=None,
-    cmap="jet",
-    vmin=-10,
-    vmax=0,
-    interpolation="none",
-    marker="*",
-    color="w",
-    markersize=15,
-    markeredgewidth=1.5,
-    markeredgecolor="k",
-):
-
-    if ax is None:
-        ax = plt.gca()
-
-    src_z_ind, src_r_ind = np.unravel_index(np.argmax(B), (len(zvec), len(rvec)))
-
-    im = ax.imshow(
-        B,
-        aspect="auto",
-        extent=[min(rvec), max(rvec), min(zvec), max(zvec)],
-        origin="lower",
-        vmin=vmin,
-        vmax=vmax,
-        interpolation=interpolation,
-        cmap=cmap,
-    )
-    ax.plot(
-        rvec[src_r_ind],
-        zvec[src_z_ind],
-        marker=marker,
-        color=color,
-        markersize=markersize,
-        markeredgewidth=markeredgewidth,
-        markeredgecolor=markeredgecolor,
-    )
-    ax.invert_yaxis()
-    return ax, im
-
-
-def plot_environment():
-    zw, cw, _ = read_ssp(
-        ROOT / "Data" / "SWELLEX96" / "CTD" / "i9606.prn", 0, 3, header=None
-    )
-    zw = np.append(zw, 217)
-    cw = np.append(cw, cw[-1])
-    zb1 = np.array([np.NaN, 217, 240])
-    cb1 = np.array([np.NaN, 1572.37, 1593.0])
-    zb2 = np.array([np.NaN, 240, 1040])
-    cb2 = np.array([np.NaN, 1881, 3245])
-    z1 = np.concatenate([zw, zb1])
-    c1 = np.concatenate([cw, cb1])
-
-    upper_zlim = [240, 0]
-    lower_zlim = [1100, 240]
-    lower_clim = [1450, 1600]
-    upper_clim = [1800, 3400]
-
-    fig, axs = plt.subplots(
-        figsize=(4, 5),
-        nrows=2,
-        ncols=2,
-        gridspec_kw={"wspace": 0.05, "hspace": 0, "width_ratios": [0.67, 0.33]},
-    )
-
-    ax = axs[0, 0]
-    ax.plot(c1, z1)
-    ax.axhline(217, c="k", lw=1)
-    ax.fill_between(lower_clim, 0, 217, color="lightblue", alpha=0.15, linewidth=0)
-    ax.fill_between(lower_clim, 217, 240, color="yellow", alpha=0.15, linewidth=0)
-    ax.set_xlim(lower_clim)
-    ax.set_xlabel("Sound Speed [m/s]")
-    ax.xaxis.set_ticks_position("top")
-    ax.xaxis.set_label_position("top")
-    ax.invert_yaxis()
-    ax.set_ylim(upper_zlim)
-    ax.set_yticks([0, 50, 100, 150, 200, 217])
-    ax.set_ylabel("Depth [m]", y=0)
-    ax.spines.right.set_linestyle((0, (5, 10)))
-    ax.spines.right.set_linewidth(0.5)
-    ax.spines.bottom.set_visible(False)
-
-    ax = axs[0, 1]
-    ax.axhline(217, c="k", lw=1)
-    ax.fill_between(upper_clim, 0, 217, color="lightblue", alpha=0.15, linewidth=0)
-    ax.fill_between(upper_clim, 217, 240, color="yellow", alpha=0.15, linewidth=0)
-    props = dict(boxstyle="round", facecolor="white", alpha=0.9)
-    ax.annotate(
-        "Sediment layer\n$\\rho = 1.76\ \mathrm{g\ cm^{-3}}$\n$a=0.2\ \mathrm{dB \ km^{-1}\ Hz^{-1}}$",
-        xy=(2500, 235),
-        xycoords="data",
-        xytext=(800, 185),
-        textcoords="data",
-        bbox=props,
-        arrowprops=dict(arrowstyle="->"),
-    )
-    ax.set_xlim(upper_clim)
-    ax.xaxis.set_tick_params(top=True, bottom=False)
-    ax.set_xticklabels([])
-    ax.set_ylim(upper_zlim)
-    ax.set_yticks([])
-    ax.spines.left.set_linestyle((0, (5, 10)))
-    ax.spines.left.set_linewidth(0.5)
-    ax.spines.bottom.set_visible(False)
-
-    ax = axs[1, 0]
-    ax.axhline(242, c="k", lw=1)
-    ax.axhline(1040, c="k", lw=1)
-    ax.fill_between(lower_clim, 240, 1040, color="tan", alpha=0.15, linewidth=0)
-    ax.fill_between(lower_clim, 1040, 1100, color="gray", alpha=0.15, linewidth=0)
-    ax.text(
-        1460,
-        450,
-        "Mudrock layer\n$\\rho = 2.06\ \mathrm{g\ cm^{-3}}$\n$a=0.06\ \mathrm{dB \ km^{-1}\ Hz^{-1}}$",
-        bbox=props,
-        va="center",
-    )
-    ax.annotate(
-        "Bedrock halfspace\n$c = 5200\ \mathrm{m\ s^{-1}}}$\n$\\rho = 2.66\ \mathrm{g\ cm^{-3}}$\n$a=0.02\ \mathrm{dB \ km^{-1}\ Hz^{-1}}$",
-        xy=(1500, 1090),
-        xycoords="data",
-        xytext=(1460, 940),
-        textcoords="data",
-        bbox=props,
-        arrowprops=dict(arrowstyle="->"),
-    )
-    ax.set_xlim(lower_clim)
-    ax.set_xticklabels([])
-    ax.set_ylim(lower_zlim)
-    ax.set_yticks([240, 400, 600, 800, 1040])
-    ax.spines.right.set_linestyle((0, (5, 10)))
-    ax.spines.right.set_linewidth(0.5)
-    ax.spines.top.set_visible(False)
-
-    ax = axs[1, 1]
-    ax.plot(cb2, zb2)
-    ax.axhline(242, c="k", lw=1)
-    ax.axhline(1040, c="k", lw=1)
-    ax.fill_between(upper_clim, 240, 1040, color="tan", alpha=0.15, linewidth=0)
-    ax.fill_between(upper_clim, 1040, 1100, color="gray", alpha=0.15, linewidth=0)
-    ax.set_xlim(upper_clim)
-    ax.invert_yaxis()
-    ax.set_ylim(lower_zlim)
-    ax.set_yticks([])
-    ax.spines.left.set_linestyle((0, (5, 10)))
-    ax.spines.left.set_linewidth(0.5)
-    ax.spines.top.set_visible(False)
-
-    return fig
-
-
 def plot_range():
     df = pd.read_csv(
         ROOT
@@ -647,8 +670,8 @@ def simulations_localization():
     axcol[0].set_title(TITLE, **TITLE_KW)
 
     # Column 4 - Depth Error History ===========================================
-    TITLE = "Depth error history: $\\vert\hat{R}_{src} - R_{src}\\vert$ [m]"
-    YLIM = [0, 8] # TODO: Validate this limit
+    TITLE = "Depth error history: $\\vert\hat{z}_{src} - z_{src}\\vert$ [m]"
+    YLIM = [200, 0] # TODO: Validate this limit
 
     axcol = axs[:, 3]
 
