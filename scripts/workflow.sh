be23b111481681b291e7810750336088a2adb33f
NeptuneProjects <william.f.jenkins@gmail.com>
2023-02-26 02:04:45 -0800

    Updated serial

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -2,4 +2,4 @@
 
 bash ./Source/scripts/config.sh ./Source/scripts/config.toml r s
 bash ./Source/scripts/run.sh r s serial_230217 cpu 12
-python3 ./Source/BOGP/aggregate.py ./Data/range_estimation/simulation/serial_230217
+python3 ./Source/BOGP/aggregate.py ./Data/localization/experimental/serial_full_depth
