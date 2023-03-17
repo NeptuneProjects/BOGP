c7ec343146f3a43e26ccdbc612157d56c5d29560
NeptuneProjects <william.f.jenkins@gmail.com>
2023-03-17 14:27:40 -0700

    Update sim config for next runs

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash ./Source/scripts/config.sh ./Source/scripts/config.toml r s
-bash ./Source/scripts/run.sh r s serial_230217 cpu 12
-python3 ./Source/BOGP/aggregate.py ./Data/localization/experimental/serial_full_depth
+bash ./Source/scripts/config.sh ./Source/scripts/config.toml l s
+bash ./Source/scripts/run.sh l s serial_multifreq_full cpu 12
+python3 ./Source/BOGP/aggregate.py ./Data/localization/simulation/serial_multifreq_full
