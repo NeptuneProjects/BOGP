98904f70a060a99b65c1e2620083b19d2f7d1c23
NeptuneProjects <william.f.jenkins@gmail.com>
2023-03-30 07:38:29 -0700

    Add collate step

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -3,3 +3,4 @@
 bash ./Source/scripts/config.sh ./Source/scripts/config.toml l s
 bash ./Source/scripts/run.sh l s serial_multifreq_full cpu 12
 python3 ./Source/BOGP/aggregate.py ./Data/localization/simulation/serial_multifreq_full
+python3 ./Source/BOGP/collate.py l s serial_multifreq_full3
