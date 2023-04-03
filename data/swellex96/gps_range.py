50626f2a6bca3417fe34f2dee34867ddac82fda9
NeptuneProjects <william.f.jenkins@gmail.com>
2023-04-03 02:14:32 -0700

    Update gps data functions

--- data/swellex96/gps_range.py
+++ data/swellex96/gps_range.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import csv
-import datetime
 from pathlib import Path
 
 import numpy as np
@@ -12,18 +11,12 @@ def load_range_csv(path):
     return np.loadtxt(path, delimiter=",", skiprows=1, usecols=2)
 
 
-def save_range_csv(path, data, header):
-    DATADIR = Path(
-        "/Users/williamjenkins/Research/Projects/BOGP/Data/SWELLEX96/VLA/selected"
-    )
-    BASE_TIME = datetime.datetime.strptime("96131 23:02", "%y%j %H:%M")
-
+def save_range_csv(path):
     start = pd.to_datetime(pd.Timestamp(1996, 5, 10, 23, 21, 0))
     end = pd.to_datetime(pd.Timestamp(1996, 5, 11, 0, 24, 0))
-
     t = pd.date_range(start, end=end, periods=350)
 
-    df = pd.read_fwf(DATADIR.parent.parent / "RangeEventS5" / "SproulToVLA.S5.txt")
+    df = pd.read_fwf(path.parent / "SproulToVLA.S5.txt")
     df["Datetime"] = pd.to_datetime(
         "96" + df["Jday"].astype(str) + " " + df["Time"], format="%y%j %H:%M"
     )
@@ -34,16 +27,12 @@ def save_range_csv(path, data, header):
     r_ts = np.interp(t, df.index, r)
 
     df2 = pd.DataFrame({"Time": t, "Range [km]": r_ts})
-    df2.to_csv(DATADIR / "gps_range.csv")
-
-
+    df2.to_csv(path)
 
 
 def main():
-    data = load_range_csv("/Users/williamjenkins/Research/Projects/BOGP/data/swellex96_S5_VLA/gps/gps_range.csv")
-    print(data)
-    print(data.shape)
-
+    DATADIR = Path(__file__).parents[3] / "data" / "swellex96_S5_VLA" / "gps"
+    save_range_csv(DATADIR / "gps_range2.csv")
 
 
 if __name__ == "__main__":
