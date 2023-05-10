6f009bc701e4848bd0c29b190768ba8d1d4ea3ba
NeptuneProjects <william.f.jenkins@gmail.com>
2023-05-10 09:13:44 -0500

    Add collate script

---
+++ scripts/swellex96/collate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Collate the data from the swellex96 experiment.
+# Usage:
+#   $ cd swellex96
+#   $ zsh ./scripts/swellex96/collate.sh
+
+python data/collate.py experimental serial_001
