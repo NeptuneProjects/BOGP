56402925278356231adbd8b489d2ba38a869368e
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-09 19:53:53 -0700

    Refactored based on tritonoa refactor

--- projects/swellex96_loc_tilt/data/acoustics.py
+++ projects/swellex96_loc_tilt/data/acoustics.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+# TODO: Refactor this module into a repo-level module
+
 from dataclasses import dataclass
 import logging
-import os
 from pathlib import Path
 import time
 from typing import Callable
@@ -14,18 +14,14 @@ from hydra.core.config_store import ConfigStore
 from hydra.utils import instantiate
 import numpy as np
 from omegaconf import OmegaConf
-from scipy.io import savemat
 from tritonoa.data import DataStream
 from tritonoa.io.sio import SIODataHandler
 from tritonoa.sp.processing import (
     FFTParameters,
-    FrequencyParameters,
     FrequencyPeakFindingParameters,
-    generate_complex_pressure,
+    Processor,
 )
-from tritonoa.sp.beamforming import covariance
 from tritonoa.sp.timefreq import frequency_vector
-from tqdm import tqdm
 
 log = logging.getLogger(__name__)
 
@@ -43,20 +39,12 @@ class ProcessPaths:
 
 @dataclass
 class ConversionConfig:
+    fmt: list[str]
     paths: ProcessPaths
     glob_pattern: str
     channels_to_remove: list[int]
     max_workers: int
-
-    def __post_init__(self):
-        self.paths.__post_init__()
-
-
-@dataclass
-class CovarianceConfig:
-    paths: ProcessPaths
-    frequencies: list[float]
-    num_segments: int
+    fs: int
 
     def __post_init__(self):
         self.paths.__post_init__()
@@ -81,12 +69,14 @@ class MergeConfig:
 class ProcessConfig:
     paths: ProcessPaths
     frequencies: list[float]
-    num_segments: int
+    samples_per_segment: int
+    segments_every_n: int
     fs: float
     channels_to_remove: int
     freq_finding_params: FrequencyPeakFindingParameters
     fft_params: FFTParameters
     max_workers: int
+    normalize_covariance: bool
 
     def __post_init__(self) -> None:
         self.paths.__post_init__()
@@ -96,41 +86,15 @@ class ProcessConfig:
         return frequency_vector(fs=self.fs, nfft=self.fft_params.nfft)
 
 
-
-def compute_covariance(config: CovarianceConfig) -> None:
-    log.info("Computing covariance matrices.")
-    for freq in tqdm(
-        config.frequencies,
-        total=len(config.frequencies),
-        desc="Processing frequencies",
-        unit="freq",
-        bar_format="{l_bar}{bar:20}{r_bar}{bar:-20b}",
-    ):
-        compute_covariance_worker(
-            path=config.paths.source / f"{freq:.1f}Hz",
-            timesteps=config.num_segments,
-        )
-    log.info("Covariance computation complete.")
-
-
-def compute_covariance_worker(path: os.PathLike, timesteps: int):
-    p = np.load(path / "data.npy")
-    K = np.zeros((timesteps, p.shape[1], p.shape[1]), dtype=complex)
-    for t in range(timesteps):
-        d = np.expand_dims(p[t], 1)
-        d /= np.linalg.norm(d)
-        K[t] = covariance(d)
-
-    np.save(path / "covariance.npy", K)
-
-
 def convert(config: ConversionConfig) -> None:
-    log.info("Converting SIO files to NUMPY files.")
+    log.info("Converting SIO files to specified formats.")
     handler = SIODataHandler(files=config.paths.source.glob(config.glob_pattern))
-    handler.convert_to_numpy(
+    handler.convert(
+        fmt=config.fmt,
         channels_to_remove=config.channels_to_remove,
         destination=config.paths.destination,
         max_workers=config.max_workers,
+        fs=config.fs,
     )
     log.info("Conversion complete.")
 
@@ -142,7 +106,7 @@ def merge(config: MergeConfig) -> None:
         base_time=config.base_time,
         start=config.start,
         end=config.end,
-        fs=config.fs,
+        fs=config.fs, 
         channels_to_remove=config.channels_to_remove,
         savepath=config.paths.destination / config.filename,
     )
@@ -151,63 +115,28 @@ def merge(config: MergeConfig) -> None:
 
 def process(config: ProcessConfig) -> None:
     log.info(f"Processing data for {config.frequencies} Hz.")
-    x, _ = DataStream.load(config.paths.source, exclude="t")
-    log.info(f"Loaded data with shape: {x.shape}")
+    ds = DataStream()
+    ds.load(config.paths.source, exclude="t")
+    log.info(f"Loaded data with shape: {ds.X.shape}")
+    if config.channels_to_remove is not None:
+        for channel in config.channels_to_remove:
+            ds.X[:, channel] = 0.0
+    ds.X = np.fliplr(ds.X)  # Reverse channel index
 
-    x[:, 42] = 0.0  # Remove corrupted channel
-    # x -= x.mean(axis=0) # Remove mean
-    x = np.fliplr(x)  # Reverse channel index
-
-    with tqdm(
-        total=len(config.frequencies),
-        desc="Processing frequencies",
-        unit="freq",
-        bar_format="{l_bar}{bar:20}{r_bar}{bar:-20b}",
-    ) as pbar:
-        with ThreadPoolExecutor(max_workers=config.max_workers) as executor:
-            futures = [
-                executor.submit(
-                    process_worker,
-                    data=x,
-                    num_segments=config.num_segments,
-                    freq_params=FrequencyParameters(
-                        freq=freq,
-                        fvec=config.fvec,
-                        peak_params=config.freq_finding_params,
-                    ),
-                    fft_params=config.fft_params,
-                    destination=config.paths.destination,
-                )
-                for freq in config.frequencies
-            ]
-            [pbar.update(1) for _ in as_completed(futures)]
-
-    log.info("Processing complete.")
-
-
-def process_worker(
-    data: np.ndarray,
-    num_segments: int,
-    freq_params: FrequencyParameters,
-    fft_params: FFTParameters,
-    destination: os.PathLike,
-) -> None:
-    def _save_data():
-        savepath = destination / f"{freq_params.freq:.1f}Hz"
-        savepath.mkdir(parents=True, exist_ok=True)
-        np.save(savepath / "data.npy", p)
-        np.save(savepath / "f_hist.npy", f_hist)
-        savemat(
-            savepath / f"data_{freq_params.freq}Hz.mat", {"p": p, "f": freq_params.freq}
-        )
-
-    p, f_hist = generate_complex_pressure(
-        data=data,
-        num_segments=num_segments,
-        freq_params=freq_params,
-        fft_params=fft_params,
+    processor = Processor(
+        data=ds,
+        fs=config.fs,
+        freq=config.frequencies,
+        fft_params=config.fft_params,
+        freq_finding_params=config.freq_finding_params,
     )
-    _save_data()
+    processor.process(
+        samples_per_segment=config.samples_per_segment,
+        segments_every_n=config.segments_every_n,
+        destination=config.paths.destination,
+        max_workers=config.max_workers,
+        normalize_covariance=config.normalize_covariance,
+    )
 
 
 def run_operation(operation: str) -> Callable:
