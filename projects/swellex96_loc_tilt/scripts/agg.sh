6cebdf34f595236dcfb21cc59acd8c182d26d334
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-13 22:38:19 -0700

    Combine agg scripts

--- projects/swellex96_loc_tilt/scripts/agg.sh
+++ projects/swellex96_loc_tilt/scripts/agg.sh
@@ -3,7 +3,13 @@
 # Script to aggregate the results of the swellex96 simulation
 # 
 # Usage:
-# zsh ./projects/swellex96_loc_tilt/scripts/agg.sh
+# zsh ./projects/swellex96_loc_tilt/scripts/agg.sh ../data/swellex96_S5_VLA_loc_tilt/outputs/loc_tilt/experimental/serial_005
 
-# python3 data/aggregate.py ../data/swellex96_S5_VLA/outputs/localization/simulation/serial_000
-python3 optimization/aggregate.py ../data/swellex96_S5_VLA_loc_tilt/outputs/loc_tilt/experimental/serial_003
+DIR="$1"
+MODE=$(basename $(dirname "$DIR"))
+SERIAL=$(basename $DIR)
+
+echo "Loading $MODE results for $SERIAL."
+
+python3 optimization/aggregate.py $DIR && \
+python3 projects/swellex96_loc_tilt/data/collate.py $MODE $SERIAL
--- projects/swellex96_loc_tilt/scripts/collate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Collate the data from the swellex96 experiment.
-# Usage:
-#   $ cd swellex96
-#   $ zsh ./projects/swellex96_loc_tilt/scripts/collate.sh
-
-python projects/swellex96_loc_tilt/data/collate.py experimental serial_003
