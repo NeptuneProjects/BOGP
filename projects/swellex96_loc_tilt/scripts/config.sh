3b82aa0465c1d4556bc4e848830a78f0546fbeb0
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-14 21:55:31 -0700

    Update usage

--- projects/swellex96_loc_tilt/scripts/config.sh
+++ projects/swellex96_loc_tilt/scripts/config.sh
@@ -2,10 +2,10 @@
 # 
 # Example usage:
 # zsh ./projects/swellex96_loc_tilt/scripts/config.sh serial_002 simulation
-# zsh ./projects/swellex96_loc_tilt/scripts/config.sh serial_006 experimental
+# zsh ./projects/swellex96_loc_tilt/scripts/config.sh serial_007 experimental
 
 SERIAL="$1"
 MODE="$2"
 
-python3 ./projects/swellex96_loc_tilt/data/configure.py serial=$SERIAL parameterization=$MODE "optimizers=[gpei]"
+python3 ./projects/swellex96_loc_tilt/data/configure.py serial=$SERIAL parameterization=$MODE
 # python3 ./projects/swellex96_loc_tilt/data/configure.py serial=$SERIAL parameterization=$MODE "optimizers=[qgpei]"
