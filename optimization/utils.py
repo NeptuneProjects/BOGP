cb96802c6c151f32492f0301abc1459865cab5d6
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-14 21:53:28 -0700

    tmp fix

--- optimization/utils.py
+++ optimization/utils.py
@@ -57,13 +57,13 @@ def get_covariance_matrix_paths(freq: list, path: Path) -> list[Path]:
     return [path / f"{f:.1f}Hz" / "covariance.npy" for f in freq]
 
 
-def adjust_bounds(lower: float, lower_bound: float, upper: float, upper_bound: float) -> float:
+def adjust_bounds(lower: float, lower_bound: float, upper: float, upper_bound: float) -> tuple[float]:
     if lower < lower_bound:
         new_lower = lower_bound
         new_upper = new_lower + abs(lower) + abs(upper)
     elif upper > upper_bound:
         new_upper = upper_bound
-        new_lower = new_upper - abs(lower) - abs(upper)
+        new_lower = new_upper - (abs(upper) - abs(lower))
     else:
         new_lower = lower
         new_upper = upper
