28ce2b51ede7146f049663cd358d84c461d9c62f
NeptuneProjects <william.f.jenkins@gmail.com>
2023-08-25 16:44:47 -0700

    Simplify

--- projects/swellex96_loc_tilt/scripts/process.sh
+++ projects/swellex96_loc_tilt/scripts/process.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-python3 ./projects/swellex96_loc_tilt/data/acoustics.py process
-# python3 ./projects/swellex96_loc_tilt/data/acoustics.py process --freqs 201,235,283,338,388
-# python3 ./data/swellex96/acoustics.py ambsurf --freqs 148,166,201,235,283,338,388
-# python3 ./data/swellex96/acoustics.py multifreq --freqs 148,166,201,235,283,338,388
+HYDRA_FULL_ERROR=1 python3 ./projects/swellex96_loc_tilt/data/acoustics.py
