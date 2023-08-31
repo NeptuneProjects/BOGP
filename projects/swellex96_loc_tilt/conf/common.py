e62b5c75e4e15c4d33325afd58b8aec79454ab60
William Jenkins <wjenkins@ucsd.edu>
2023-08-31 10:00:24 -0700

    Update data folder

--- projects/swellex96_loc_tilt/conf/common.py
+++ projects/swellex96_loc_tilt/conf/common.py
@@ -27,6 +27,6 @@ class SWELLEX96Paths:
     path = SWELLEX96PATH
     environment_data = SWELLEX96PATH / "env_models" / "swellex96.json"
     gps_data = SWELLEX96PATH / "gps" / "source_tow.csv"
-    acoustic_path = SWELLEX96PATH / "acoustic" / "processed"
+    acoustic_path = SWELLEX96PATH / "acoustic" / "processed_002"
     outputs = SWELLEX96PATH / "outputs"
     ambiguity_surfaces = SWELLEX96PATH / "acoustic" / "ambiguity_surfaces"
