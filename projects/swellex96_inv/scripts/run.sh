925b7be8e74a0864b7647f8c25072043ffa8de80
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-15 21:22:18 -0500

    Add run script

---
+++ projects/swellex96_inv/scripts/run.sh
@@ -0,0 +1,5 @@
+python projects/swellex96_inv/data/bo/run.py --init=400
+python projects/swellex96_inv/data/bo/run.py --init=300
+python projects/swellex96_inv/data/bo/run.py --init=200
+python projects/swellex96_inv/data/bo/run.py --init=100
+python projects/swellex96_inv/data/bo/run.py --init=50
