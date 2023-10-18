ccd9a0733de1d7f4b318a6e94a4706fab5dd25e2
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-18 13:15:22 -0700

    Update env

--- projects/swellex96_inv/data/env.py
+++ projects/swellex96_inv/data/env.py
@@ -82,27 +82,15 @@ def format_ssp(z_data: np.ndarray, c_data: np.ndarray) -> tuple[np.ndarray, np.n
     # To be estimated:
     ind2 = np.argmin(np.abs(z_data - 5))
     z2, c2 = z_data[ind2], c_data[ind2]
-    # ind3 = np.argmin(np.abs(z_data - 15))
-    # z3, c3 = z_data[ind3], c_data[ind3]
+    ind3 = np.argmin(np.abs(z_data - 15))
+    z3, c3 = z_data[ind3], c_data[ind3]
     ind4 = np.argmin(np.abs(z_data - 30))
     z4, c4 = z_data[ind4], c_data[ind4]
     ind5 = np.argmin(np.abs(z_data - 60))
     z5, c5 = z_data[ind5], c_data[ind5]
     ind6 = np.argmin(np.abs(z_data - 100))
     z6, c6 = z_data[ind6], c_data[ind6]
-    # return np.array([z1, z2, z3, z4, z5, z6]), np.array([c1, c2, c3, c4, c5, c6])
-    return np.array([z1, z2, z4, z5, z6]), np.array([c1, c2, c4, c5, c6])
-
-    # ind2 = np.argmin(np.abs(z_data - 10))
-    # z2, c2 = z_data[ind2], c_data[ind2]
-    # ind3 = np.argmin(np.abs(z_data - 30))
-    # z3, c3 = z_data[ind3], c_data[ind3]
-    # ind4 = np.argmin(np.abs(z_data - 60))
-    # z4, c4 = z_data[ind4], c_data[ind4]
-    # ind5 = np.argmin(np.abs(z_data - 100))
-    # z5, c5 = z_data[ind5], c_data[ind5]
-
-    # return np.array([z1, z2, z3, z4, z5]), np.array([c1, c2, c3, c4, c5])
+    return np.array([z1, z2, z3, z4, z5, z6]), np.array([c1, c2, c3, c4, c5, c6])
 
 
 def build_simple_environment(
@@ -111,12 +99,7 @@ def build_simple_environment(
     model: str = "KRAKEN",
     tilt: Optional[float] = None,
 ) -> dict:
-    z_data, c_data = load_ctd_data(ctd_path)
-    # slice_every = 60
-    # if slice_every:
-    #     z_data = z_data[::slice_every]
-    #     c_data = c_data[::slice_every]
-    z_data, c_data = format_ssp(z_data, c_data)
+    z_data, c_data = format_ssp(*load_ctd_data(ctd_path))
     print(np.diff(c_data))
     z_data = np.append(z_data, 217).tolist()
     c_data = np.append(c_data, c_data[-1]).tolist()
@@ -127,15 +110,14 @@ def build_simple_environment(
         # 2. Top medium (halfspace) - Not used
         # 3. Layered media
         "layerdata": [
-            # {"z": [0.0, 35.0, 100.0, 217.0], "c_p": [1522.0, 1493.4, 1488.4, 1488.9], "rho": 1.0},
             {"z": z_data, "c_p": c_data, "rho": 1.0},
-            # {"z": [217.0, 240.0], "c_p": [1572.3, 1593.0], "rho": 1.76, "a_p": 0.2},
+            {"z": [217.0, 240.0], "c_p": [1572.3, 1593.0], "rho": 1.76, "a_p": 0.2},
         ],
         # 4. Bottom medium
         "bot_opt": "A",
-        "bot_c_p": 1572.3,
-        "bot_rho": 1.76,
-        "bot_a_p": 0.2,
+        "bot_c_p": 1881.0,
+        "bot_rho": 2.06,
+        "bot_a_p": 0.06,
         # "bot_c_p": 1881.0,
         # "bot_rho": 2.06,
         # "bot_a_p": 0.06,
