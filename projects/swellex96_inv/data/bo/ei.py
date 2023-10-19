Fix objective function sign

projects/swellex96_inv/data/bo/ei.py
@@ -13,7 +13,7 @@ from gpytorch.likelihoods import GaussianLikelihood
 from gpytorch.mlls import ExactMarginalLogLikelihood
 import torch
 
-import helpers
+import common, helpers
 
 
 @dataclass
@@ -42,17 +42,18 @@ def loop(
     **kwargs,
 ) -> tuple[torch.tensor, torch.tensor]:
     logging.info(f"Running GP/EI on {device.type.upper()}.")
-    
+
     start = time.time()
-    
+
     X = helpers.get_initial_points(dim, n_init, dtype, device, seed)
-    Y = torch.tensor(objective(X.detach().cpu().numpy()), dtype=dtype, device=device)
+    Y = -torch.tensor(objective(X.detach().cpu().numpy()), dtype=dtype, device=device)
 
     stop = time.time() - start
     times = [stop / n_init for _ in range(n_init)]
-    
-    logging.info(
-        f"{n_init} warmup trials complete. | Best value: {1 - Y.max().item():.3}"
+
+    logging.info(f"{n_init} warmup trials complete.")
+    helpers.log_best_value_and_parameters(
+        X.detach().cpu().numpy(), -Y.detach().cpu().numpy(), common.SEARCH_SPACE
     )
     logging.info("Commencing Bayesian optimization.")
 
@@ -77,7 +78,7 @@ def loop(
 
             # Create a batch
             ei = ExpectedImprovement(model, train_Y.max())
-            candidate, acq_value = optimize_acqf(
+            candidate, _ = optimize_acqf(
                 ei,
                 bounds=torch.stack(
                     [
@@ -89,7 +90,7 @@ def loop(
                 num_restarts=num_restarts,
                 raw_samples=raw_samples,
             )
-            Y_next = torch.tensor(
+            Y_next = -torch.tensor(
                 objective(candidate.detach().cpu().numpy()),
                 dtype=dtype,
                 device=device,
@@ -102,7 +103,13 @@ def loop(
             times.append(time.time() - start)
 
             # Print current status
-            logging.info(f"Trial {len(X)} | Best value: {1 - Y.max().item():.3}")
+            print("-" * 100)
+            logging.info(f"GP/EI | Trial {len(X)}")
+            helpers.log_current_value_and_parameters(
+                X.detach().cpu().numpy(), -Y.detach().cpu().numpy(), common.SEARCH_SPACE
+            )
+            helpers.log_best_value_and_parameters(
+                X.detach().cpu().numpy(), -Y.detach().cpu().numpy(), common.SEARCH_SPACE
+            )
 
-    logging.info(f"Complete; best parameters: {X[Y.argmax()]}")
-    return X, Y, times
+    return X, -Y, times

projects/swellex96_inv/data/bo/obj.py
@@ -13,12 +13,13 @@ from tritonoa.sp.mfp import MatchedFieldProcessor
 from tritonoa.sp.processing import simulate_covariance
 
 sys.path.insert(0, str(Path(__file__).parents[2]))
-from conf import common
+# from conf import common
 from data import formatter
 
 sys.path.insert(0, str(Path(__file__).parents[4]))
 from optimization import utils
 
+import common
 
 def convert_tensor_to_parameters(x: np.ndarray) -> dict:
     return [
@@ -28,8 +29,7 @@ def convert_tensor_to_parameters(x: np.ndarray) -> dict:
 
 def evaluate_objective(objective: callable, parameters: list[dict]) -> list[float]:
     with ThreadPoolExecutor(max_workers=min(len(parameters), 64)) as executor:
-        results = executor.map(objective, parameters)
-    return [1 - result for result in results]
+        return list(executor.map(objective, parameters))
 
 
 def get_bounds_from_search_space(search_space: list[dict]) -> np.ndarray:
@@ -37,7 +37,6 @@ def get_bounds_from_search_space(search_space: list[dict]) -> np.ndarray:
 
 
 def get_objective(simulate: bool = True) -> MatchedFieldProcessor:
-    base_env = utils.load_env_from_json(common.SWELLEX96Paths.main_environment_data)
     if simulate:
         K = simulate_covariance(
             runner=run_kraken,
@@ -60,7 +59,7 @@ def get_objective(simulate: bool = True) -> MatchedFieldProcessor:
         runner=run_kraken,
         covariance_matrix=K,
         freq=common.FREQ,
-        parameters=base_env,
+        parameters=utils.load_env_from_json(common.SWELLEX96Paths.simple_environment_data),
         parameter_formatter=formatter.format_parameters,
         beamformer=partial(beamformer, atype="cbf_ml"),
         multifreq_method="product",

projects/swellex96_inv/data/bo/run.py
@@ -12,12 +12,13 @@ import random
 import sys
 import warnings
 
+import matplotlib.pyplot as plt
 import numpy as np
 
-import baxus, ei, gibbon, helpers, obj, pi, sobol, ucb, grid
+import baxus, ei, gibbon, helpers, obj, pi, sobol, ucb, grid, common
 
-sys.path.insert(0, str(Path(__file__).parents[2]))
-from conf import common
+# sys.path.insert(0, str(Path(__file__).parents[2]))
+# from conf import common
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -84,7 +85,7 @@ def main(args) -> None:
     random.seed(args.seed)
     seeds = helpers.get_random_seeds(args.runs)
 
-    print("_" * 80)
+    print("=" * 100)
     for seed in seeds:
         serial_name = f"{args.optim}_{args.budget}-{args.init}_{seed:04d}"
         helpers.initialize_logger_file(args.dir / f"{serial_name}.log", logger, logfmt)
@@ -111,16 +112,17 @@ def main(args) -> None:
             t=times,
         )
 
-        best_params = X[np.argmin(Y, axis=0)]
-        best_params = helpers.transform_to_original_space(best_params, common.SEARCH_SPACE)
-        logger.info("Best parameters:")
-        logger.info(
-            "".join([f"{d['name']}: {v:.2f} | " for d, v in zip(common.SEARCH_SPACE, best_params.squeeze())])[:-3]
-        )
+        print("-" * 100)
+        logger.info("*** Optimization complete. ***")
+        helpers.log_best_value_and_parameters(X, Y, common.SEARCH_SPACE)
+        # logger.info("Best parameters:")
+        # logger.info(
+        #     "".join([f"{d['name']}: {v:.2f} | " for d, v in zip(common.SEARCH_SPACE, best_params.squeeze())])[:-3]
+        # )
         
         logger.handlers[1].stream.close()
         logger.removeHandler(logger.handlers[1])
-        print("_" * 80)
+        print("=" * 100)
 
         if args.optim == Strategy.GRID:
             break