3a10cbd4d60f66fdc6625afd6b912429eecb0bba
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-20 15:24:16 -0700

    Update device

--- projects/swellex96_inv/scripts/run_gpucb.sh
+++ projects/swellex96_inv/scripts/run_gpucb.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=400
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=300
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=100
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=50
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=400
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=300
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=100
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=50
