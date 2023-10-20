c2aba4d7feb9e55c37f8248a91e1e2a875b2f10e
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-19 19:51:23 -0700

    Add runner script

---
+++ projects/swellex96_inv/scripts/run_ucb.sh
@@ -0,0 +1,5 @@
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=400
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=300
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=100
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=50
