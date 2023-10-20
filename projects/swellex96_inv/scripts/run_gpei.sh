bd4fc08396fb6287341e99e719381034d0018e28
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-20 15:19:49 -0700

    Update devices

--- projects/swellex96_inv/scripts/run_gpei.sh
+++ projects/swellex96_inv/scripts/run_gpei.sh
@@ -1,5 +1,5 @@
-python projects/swellex96_inv/data/bo/run.py --init=400
-python projects/swellex96_inv/data/bo/run.py --init=300
-python projects/swellex96_inv/data/bo/run.py --init=200
-python projects/swellex96_inv/data/bo/run.py --init=100
-python projects/swellex96_inv/data/bo/run.py --init=50
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=400
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=300
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=200
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=100
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=50
