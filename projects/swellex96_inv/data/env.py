8043c33cd887d9c388651ba1b830313020912505
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-19 17:17:08 -0700

    Update depths

--- projects/swellex96_inv/data/env.py
+++ projects/swellex96_inv/data/env.py
@@ -82,11 +82,11 @@ def format_ssp(z_data: np.ndarray, c_data: np.ndarray) -> tuple[np.ndarray, np.n
     # To be estimated:
     ind2 = np.argmin(np.abs(z_data - 5))
     z2, c2 = z_data[ind2], c_data[ind2]
-    ind3 = np.argmin(np.abs(z_data - 15))
+    ind3 = np.argmin(np.abs(z_data - 20))
     z3, c3 = z_data[ind3], c_data[ind3]
-    ind4 = np.argmin(np.abs(z_data - 30))
+    ind4 = np.argmin(np.abs(z_data - 40))
     z4, c4 = z_data[ind4], c_data[ind4]
-    ind5 = np.argmin(np.abs(z_data - 60))
+    ind5 = np.argmin(np.abs(z_data - 70))
     z5, c5 = z_data[ind5], c_data[ind5]
     ind6 = np.argmin(np.abs(z_data - 100))
     z6, c6 = z_data[ind6], c_data[ind6]
