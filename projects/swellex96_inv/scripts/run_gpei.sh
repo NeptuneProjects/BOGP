fc1bcc051012cadc9240fcaf4723ed0336323993
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-23 11:02:36 -0700

    Update runs

--- projects/swellex96_inv/scripts/run_gpei.sh
+++ projects/swellex96_inv/scripts/run_gpei.sh
@@ -1,5 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=400
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=300
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=200
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=100
-CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=50
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=sim_ei --simulate
+
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=400
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=300
+CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=exp_ei
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=100
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=50
--- projects/swellex96_inv/scripts/run_gpucb.sh
+++ projects/swellex96_inv/scripts/run_gpucb.sh
@@ -1,5 +1,37 @@
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=400
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=300
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=100
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=50
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=400 --serial=exp_ucb_b1
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=300 --serial=exp_ucb_b1
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200 --serial=sim_ucb_b1 --simulate
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=100 --serial=exp_ucb_b1
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=50 --serial=exp_ucb_b1
+
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=400 --serial=exp_ucb_b01
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=300 --serial=exp_ucb_b01
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=200 --serial=sim_ucb_b01 --simulate
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=100 --serial=exp_ucb_b01
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=50 --serial=exp_ucb_b01
+
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=400 --serial=exp_ucb_b5
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=300 --serial=exp_ucb_b5
+CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=200 --serial=sim_ucb_b5 --simulate
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=100 --serial=exp_ucb_b5
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=50 --serial=exp_ucb_b5
+
+
+
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=400 --serial=exp_ucb_b1
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=300 --serial=exp_ucb_b1
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200 --serial=exp_ucb_b1
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=100 --serial=exp_ucb_b1
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=50 --serial=exp_ucb_b1
+
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=400 --serial=exp_ucb_b01
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=300 --serial=exp_ucb_b01
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=200 --serial=exp_ucb_b01
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=100 --serial=exp_ucb_b01
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=0.1 --init=50 --serial=exp_ucb_b01
+
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=400 --serial=exp_ucb_b5
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=300 --serial=exp_ucb_b5
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=200 --serial=exp_ucb_b5
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=100 --serial=exp_ucb_b5
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=ucb --beta=5 --init=50 --serial=exp_ucb_b5
