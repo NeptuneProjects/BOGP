c89a0cb32907ce0d734b5f2257625004c0b116f4
NeptuneProjects <william.f.jenkins@gmail.com>
2023-10-24 08:17:44 -0700

    Update progress

--- projects/swellex96_inv/scripts/run_gpei.sh
+++ projects/swellex96_inv/scripts/run_gpei.sh
@@ -5,7 +5,7 @@
 # CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=50
 
 # In progress
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=sim_ei --simulate
 CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=exp_ei
 
 # Completed
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=sim_ei --simulate
