01ed968eb6ec9b753602c394e52a4d568870899d
William Jenkins <wfjenkins@ucsd.edu>
2024-04-22 16:04:01 +0900

    Comment out unused

--- projects/swellex96_inv/visualization/param_est.py
+++ projects/swellex96_inv/visualization/param_est.py
@@ -47,135 +47,18 @@ def plot_parameter_estimates(data: list[pd.DataFrame]) -> plt.Figure:
             ax.grid(True, linestyle="dotted")
 
             param_name = common.SEARCH_SPACE[j]["name"]
+            param_label = common.VARIABLES[common.SEARCH_SPACE[j]["name"]]
             bounds = common.SEARCH_SPACE[j]["bounds"]
             true_value = common.TRUE_VALUES[common.SEARCH_SPACE[j]["name"]]
             if param_name == "dc_p_sed":
                 param_name = "c_p_sed_bot"
+                param_label = common.VARIABLES[param_name]
                 bounds = [1540, 1690]
                 true_value = 1593.0
 
             ax = plot_param_dist(df[f"best_{param_name}"], bounds, ax=ax)
 
             ax.text(0, 1.07, f"({ascii_lowercase[7 * i + j]})", transform=ax.transAxes)
-            ax.axvline(
-                true_value,
-                color="k",
-                linestyle="dashed",
-            )
-            ax.set_xlim(bounds)
-            ax.set_xticklabels([]) if i != 1 else None
-            ax.set_xlabel(
-                common.VARIABLES[common.SEARCH_SPACE[j]["name"]]
-            ) if i == 1 else None
-            ax.set_ylim(YLIM[i])
-            ax.set_yticklabels([]) if j != 0 else None
-            ax.set_ylabel(f"{MODES[i]}\nCount") if j == 0 else None
-            helpers.adjust_subplotxticklabels(ax, 0, -1)
-
-    return fig
-
-
-def plot_parameter_estimates_full(data: list[pd.DataFrame]) -> plt.Figure:
-    YLIM = [[0, 20], [0, 20]]
-    NCOLS = 5
-    NROWS = 2
-
-    fig = plt.figure(figsize=(8, 4))
-    gs_outer = gridspec.GridSpec(nrows=2, ncols=1, figure=fig, hspace=0.45)
-
-    # Tranche 1 of parameters
-    gs_inner = gridspec.GridSpecFromSubplotSpec(
-        nrows=NROWS, ncols=NCOLS, subplot_spec=gs_outer[0], wspace=0.1, hspace=0.4
-    )
-
-    for i in range(gs_inner.get_geometry()[0]):
-        for j in range(gs_inner.get_geometry()[1]):
-            ax = fig.add_subplot(gs_inner[i, j])
-
-            ax.grid(True, linestyle="dotted")
-
-            param_name = common.SEARCH_SPACE[j]["name"]
-            bounds = common.SEARCH_SPACE[j]["bounds"]
-            true_value = common.TRUE_VALUES[common.SEARCH_SPACE[j]["name"]]
-            if param_name == "dc_p_sed":
-                param_name = "c_p_sed_bot"
-                bounds = [1540, 1690]
-                true_value = 1593.0
-
-            ax = plot_param_dist(data[i][f"best_{param_name}"], bounds, ax=ax)
-
-            ax.text(
-                0, 1.07, f"({ascii_lowercase[NCOLS * i + j]})", transform=ax.transAxes
-            )
-            ax.axvline(
-                true_value,
-                color="k",
-                linestyle="dashed",
-            )
-            ax.set_xlim(bounds)
-            ax.set_xticklabels([]) if i != 1 else None
-            ax.set_xlabel(
-                common.VARIABLES[common.SEARCH_SPACE[j]["name"]]
-            ) if i == 1 else None
-            ax.set_ylim(YLIM[i])
-            ax.set_yticklabels([]) if j != 0 else None
-            ax.set_ylabel(f"{MODES[i]}\nCount") if j == 0 else None
-            helpers.adjust_subplotxticklabels(ax, 0, -1)
-
-    # Tranche 2 of parameters
-    gs_inner = gridspec.GridSpecFromSubplotSpec(
-        nrows=NROWS, ncols=NCOLS, subplot_spec=gs_outer[1], wspace=0.1, hspace=0.4
-    )
-
-    for i in range(gs_inner.get_geometry()[0]):
-        for j in range(gs_inner.get_geometry()[1] - 1):
-            ax = fig.add_subplot(gs_inner[i, j])
-
-            ax.grid(True, linestyle="dotted")
-
-            param_name = common.SEARCH_SPACE[j + NCOLS]["name"]
-            param_label = common.VARIABLES[common.SEARCH_SPACE[j + NCOLS]["name"]]
-            bounds = common.SEARCH_SPACE[j + NCOLS]["bounds"]
-            true_value = common.TRUE_VALUES[common.SEARCH_SPACE[j + NCOLS]["name"]]
-            if param_name == "dc_p_sed":
-                param_name = "c_p_sed_bot"
-                param_label = common.VARIABLES[param_name]
-                bounds = [1540.0, 1690.0]
-                true_value = 1593.0
-            # if param_name == "dc1":
-            #     param_name = "c2"
-            #     param_label = common.VARIABLES[param_name]
-            #     bounds = [1480, 1540]
-            #     true_value = 1499.833
-            # if param_name == "dc2":
-            #     param_name = "c3"
-            #     param_label = common.VARIABLES[param_name]
-            #     bounds = [1480, 1540]
-            #     true_value = 1492.56
-            if param_name == "dc3":
-                param_name = "c4"
-                param_label = common.VARIABLES[param_name]
-                bounds = [1460.0, 1520.0]
-                true_value = 1488.0
-                print(data[i][f"best_{param_name}"])
-            # if param_name == "dc4":
-            #     param_name = "c5"
-            #     param_label = common.VARIABLES[param_name]
-            #     bounds = [1480, 1540]
-            #     true_value = 1489.663
-            # if param_name == "dc5":
-            #     param_name = "c6"
-            #     param_label = common.VARIABLES[param_name]
-            #     bounds = [1480, 1540]
-            #     true_value = 1488.434
-
-            ax = plot_param_dist(data[i][f"best_{param_name}"], bounds, ax=ax)
-            ax.text(
-                0,
-                1.07,
-                f"({ascii_lowercase[NCOLS * i + j + 10]})",
-                transform=ax.transAxes,
-            )
             ax.axvline(
                 true_value,
                 color="k",
@@ -190,6 +73,123 @@ def plot_parameter_estimates_full(data: list[pd.DataFrame]) -> plt.Figure:
             helpers.adjust_subplotxticklabels(ax, 0, -1)
 
     return fig
+
+
+# def plot_parameter_estimates_full(data: list[pd.DataFrame]) -> plt.Figure:
+#     YLIM = [[0, 20], [0, 20]]
+#     NCOLS = 7
+#     NROWS = 2
+
+#     fig = plt.figure(figsize=(8, 4))
+#     gs_outer = gridspec.GridSpec(nrows=2, ncols=1, figure=fig, hspace=0.45)
+
+#     # Tranche 1 of parameters
+#     gs_inner = gridspec.GridSpecFromSubplotSpec(
+#         nrows=NROWS, ncols=NCOLS, subplot_spec=gs_outer[0], wspace=0.1, hspace=0.4
+#     )
+
+#     for i in range(gs_inner.get_geometry()[0]):
+#         for j in range(gs_inner.get_geometry()[1]):
+#             ax = fig.add_subplot(gs_inner[i, j])
+
+#             ax.grid(True, linestyle="dotted")
+
+#             param_name = common.SEARCH_SPACE[j]["name"]
+#             bounds = common.SEARCH_SPACE[j]["bounds"]
+#             true_value = common.TRUE_VALUES[common.SEARCH_SPACE[j]["name"]]
+#             if param_name == "dc_p_sed":
+#                 param_name = "c_p_sed_bot"
+#                 bounds = [1540, 1690]
+#                 true_value = 1593.0
+
+#             ax = plot_param_dist(data[i][f"best_{param_name}"], bounds, ax=ax)
+
+#             ax.text(
+#                 0, 1.07, f"({ascii_lowercase[NCOLS * i + j]})", transform=ax.transAxes
+#             )
+#             ax.axvline(
+#                 true_value,
+#                 color="k",
+#                 linestyle="dashed",
+#             )
+#             ax.set_xlim(bounds)
+#             ax.set_xticklabels([]) if i != 1 else None
+#             ax.set_xlabel(
+#                 common.VARIABLES[common.SEARCH_SPACE[j]["name"]]
+#             ) if i == 1 else None
+#             ax.set_ylim(YLIM[i])
+#             ax.set_yticklabels([]) if j != 0 else None
+#             ax.set_ylabel(f"{MODES[i]}\nCount") if j == 0 else None
+#             helpers.adjust_subplotxticklabels(ax, 0, -1)
+
+#     # Tranche 2 of parameters
+#     gs_inner = gridspec.GridSpecFromSubplotSpec(
+#         nrows=NROWS, ncols=NCOLS, subplot_spec=gs_outer[1], wspace=0.1, hspace=0.4
+#     )
+
+#     for i in range(gs_inner.get_geometry()[0]):
+#         for j in range(gs_inner.get_geometry()[1] - 1):
+#             ax = fig.add_subplot(gs_inner[i, j])
+
+#             ax.grid(True, linestyle="dotted")
+
+#             param_name = common.SEARCH_SPACE[j + NCOLS]["name"]
+#             param_label = common.VARIABLES[common.SEARCH_SPACE[j + NCOLS]["name"]]
+#             bounds = common.SEARCH_SPACE[j + NCOLS]["bounds"]
+#             true_value = common.TRUE_VALUES[common.SEARCH_SPACE[j + NCOLS]["name"]]
+#             if param_name == "dc_p_sed":
+#                 param_name = "c_p_sed_bot"
+#                 param_label = common.VARIABLES[param_name]
+#                 bounds = [1540.0, 1690.0]
+#                 true_value = 1593.0
+#             # if param_name == "dc1":
+#             #     param_name = "c2"
+#             #     param_label = common.VARIABLES[param_name]
+#             #     bounds = [1480, 1540]
+#             #     true_value = 1499.833
+#             # if param_name == "dc2":
+#             #     param_name = "c3"
+#             #     param_label = common.VARIABLES[param_name]
+#             #     bounds = [1480, 1540]
+#             #     true_value = 1492.56
+#             if param_name == "dc3":
+#                 param_name = "c4"
+#                 param_label = common.VARIABLES[param_name]
+#                 bounds = [1460.0, 1520.0]
+#                 true_value = 1488.0
+#                 print(data[i][f"best_{param_name}"])
+#             # if param_name == "dc4":
+#             #     param_name = "c5"
+#             #     param_label = common.VARIABLES[param_name]
+#             #     bounds = [1480, 1540]
+#             #     true_value = 1489.663
+#             # if param_name == "dc5":
+#             #     param_name = "c6"
+#             #     param_label = common.VARIABLES[param_name]
+#             #     bounds = [1480, 1540]
+#             #     true_value = 1488.434
+
+#             ax = plot_param_dist(data[i][f"best_{param_name}"], bounds, ax=ax)
+#             ax.text(
+#                 0,
+#                 1.07,
+#                 f"({ascii_lowercase[NCOLS * i + j + 10]})",
+#                 transform=ax.transAxes,
+#             )
+#             ax.axvline(
+#                 true_value,
+#                 color="k",
+#                 linestyle="dashed",
+#             )
+#             ax.set_xlim(bounds)
+#             ax.set_xticklabels([]) if i != 1 else None
+#             ax.set_xlabel(param_label) if i == 1 else None
+#             ax.set_ylim(YLIM[i])
+#             ax.set_yticklabels([]) if j != 0 else None
+#             ax.set_ylabel(f"{MODES[i]}\nCount") if j == 0 else None
+#             helpers.adjust_subplotxticklabels(ax, 0, -1)
+
+#     return fig
 
 
 def main(strategy="ei", prepend=""):
@@ -228,4 +228,5 @@ def main(strategy="ei", prepend=""):
 
 
 if __name__ == "__main__":
-    main(prepend="thermo_")
+    fig = main(prepend="")
+    fig.savefig("test_dist.png", dpi=300, bbox_inches="tight")
