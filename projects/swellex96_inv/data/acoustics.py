3f2804ac79936f3a900799580f2ec7ad14842102
William Jenkins <wfjenkins@ucsd.edu>
2024-04-22 17:26:32 +0900

    ts complete

--- projects/swellex96_inv/data/acoustics.py
+++ projects/swellex96_inv/data/acoustics.py
@@ -88,9 +88,6 @@ class ProcessConfig:
 
 def convert(config: ConversionConfig) -> None:
     log.info("Converting SIO files to specified formats.")
-    print(config.paths.source)
-    print(config.glob_pattern)
-    print(list(config.paths.source.glob(config.glob_pattern)))
     handler = SIODataHandler(files=config.paths.source.glob(config.glob_pattern))
     handler.convert(
         fmt=config.fmt,
