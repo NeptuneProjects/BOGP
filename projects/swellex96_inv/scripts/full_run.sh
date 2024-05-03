ef8323ef843faac7c6b8872bc2a961b52e900a27
William Jenkins <wfjenkins@ucsd.edu>
2024-05-02 23:20:25 -0700

    Re-order routines

--- projects/swellex96_inv/scripts/full_run.sh
+++ projects/swellex96_inv/scripts/full_run.sh
@@ -1,7 +1,3 @@
-# SOBOL
-python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=exp_sobol
-python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=sim_sobol --simulate
-
 # BOGP - UCB
 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200 --serial=sim_ucb --simulate
 python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200 --serial=exp_ucb
@@ -13,3 +9,7 @@ python projects/swellex96_inv/data/bo/run.py --optim=ei --init=400 --serial=exp_
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=300 --serial=exp_ei
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=100 --serial=exp_ei
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=50 --serial=exp_ei
+
+# SOBOL
+# python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=exp_sobol
+# python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=sim_sobol --simulate
