3b8f2fd63edcd74dd8f8d3f6cd072b9421057ee8
William Jenkins <wfjenkins@ucsd.edu>
2024-05-09 01:06:59 -0700

    Update fig to account for MC runs

--- projects/swellex96_inv/visualization/diff_ev.py
+++ projects/swellex96_inv/visualization/diff_ev.py
@@ -6,13 +6,18 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import scienceplots
+import seaborn as sns
 
 sys.path.insert(0, str(Path(__file__).parents[1]))
 from data.bo import common
 
 plt.style.use(["science", "ieee", "std-colors"])
 
+YLIM = (0.07, 1.0)
+YTICKS = [0.07, 0.1, 0.5, 1.0]
+YTICKLABELS = ["", "0.1", "0.5", "1"]
 
 def load_de_results(path: Path) -> tuple:
     data = np.load(path)
@@ -20,9 +25,16 @@ def load_de_results(path: Path) -> tuple:
 
 
 def main():
-    path = common.SWELLEX96Paths.outputs / "de_results_exp.npz"
-    nit, nfev, elapsed_time, fun = load_de_results(path)
+    path = common.SWELLEX96Paths.outputs / "runs" / "de" / "de_results.csv"
+    df = pd.read_csv(path)
 
+    nit = df[df["seed"] == 0]["nit"].values
+    nfev = df[df["seed"] == 0]["nfev"].values
+    time = df.pivot(index="nit", columns="seed", values="wall_time").mean(axis=1).values
+    mean_ = df.pivot(index="nit", columns="seed", values="obj").mean(axis=1)
+    min_ = df.pivot(index="nit", columns="seed", values="obj").min(axis=1)
+    max_ = df.pivot(index="nit", columns="seed", values="obj").max(axis=1)
+    
     def nit2nfev(x):
         return np.interp(
             x,
@@ -34,29 +46,34 @@ def main():
         return np.interp(x, nfev, nit)
 
     def nit2time(x):
-        return np.interp(x, nit, elapsed_time)
+        return np.interp(x, nit, time)
 
     def time2nit(x):
-        return np.interp(x, elapsed_time, nit)
+        return np.interp(x, time, nit)
 
-    fig, ax = plt.subplots(figsize=(3, 1.5), nrows=1)
+    fig, ax = plt.subplots(figsize=(3, 2), nrows=1)
 
-    ax0 = ax
-    ax0.plot(nit, fun, label="$\widehat{\phi}$")
-    ax0.text(-0.025, -0.04, "DE iter.", ha="right", va="top", transform=ax0.transAxes)
-    ax0.set_ylabel("$\widehat{\phi}$", rotation=0)
-    ax0.set_xlim(0, 40)
-    ax0.grid()
+    ax.plot(nit, mean_, color="k")
+    ax.plot(nit, min_, color="k", linestyle="-.", linewidth=0.5)
+    ax.plot(nit, max_, color="k", linestyle="-.", linewidth=0.5)
+    ax.set_xlim(0, max(nit))
+    ax.set_yscale("log")
+    ax.set_ylim(YLIM)
+    ax.set_yticks(YTICKS)
+    ax.set_yticklabels(YTICKLABELS)
+    ax.text(-0.025, -0.04, "DE iter.", ha="right", va="top", transform=ax.transAxes)
+    ax.set_ylabel("$\widehat{\phi}$", rotation=0)
+    ax.grid()
 
-    ax1 = ax0.secondary_xaxis(location=-0.2, functions=(nit2nfev, nfev2nit))
+    ax1 = ax.secondary_xaxis(location=-0.2, functions=(nit2nfev, nfev2nit))
     ax1.xaxis.set_major_locator(plt.FixedLocator(np.arange(0, 25000, 5000)))
-    ax0.text(
-        -0.025, -0.24, "No. $\phi$ eval.", ha="right", va="top", transform=ax0.transAxes
+    ax.text(
+        -0.025, -0.24, "No. $\phi$ eval.", ha="right", va="top", transform=ax.transAxes
     )
 
-    ax2 = ax0.secondary_xaxis(location=-0.4, functions=(nit2time, time2nit))
-    ax2.xaxis.set_major_locator(plt.FixedLocator([0, 200, 400, 600, 800]))
-    ax0.text(-0.025, -0.44, "Time [s]", ha="right", va="top", transform=ax0.transAxes)
+    ax2 = ax.secondary_xaxis(location=-0.4, functions=(nit2time, time2nit))
+    ax2.xaxis.set_major_locator(plt.FixedLocator([0, 100, 200, 300, 400]))
+    ax.text(-0.025, -0.44, "Time [s]", ha="right", va="top", transform=ax.transAxes)
 
     return fig
 
