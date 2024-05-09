223ee53b89b6e6a0c273dfca1a014e18e4abfa6d
William Jenkins <wfjenkins@ucsd.edu>
2024-05-09 01:23:54 -0700

    Update run scripts

--- projects/swellex96_inv/scripts/full_run.sh
+++ projects/swellex96_inv/scripts/full_run.sh
@@ -8,6 +8,7 @@ python projects/swellex96_inv/data/bo/run.py --optim=ei --init=200 --serial=exp_
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=400 --serial=exp_ei
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=300 --serial=exp_ei
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=100 --serial=exp_ei
+# Only one not apportioned:
 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=50 --serial=exp_ei
 
 # SOBOL
---
+++ projects/swellex96_inv/scripts/run_gpei_2.sh
@@ -0,0 +1,15 @@
+# To run
+
+CUDA_VISIBLE_DEVICES=2 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=200 --serial=sim_ei --simulate
+CUDA_VISIBLE_DEVICES=2 python projects/swellex96_inv/data/bo/run.py --optim=ei --init=200 --serial=exp_ei
+
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=400
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=300
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=100
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=50
+
+# In progress
+
+# Completed
+# CUDA_VISIBLE_DEVICES=1 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=exp_ei
+# CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --init=200 --serial=sim_ei --simulate
