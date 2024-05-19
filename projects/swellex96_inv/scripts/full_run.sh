ada492039d2a7f578a3cd2d22f06a53bf6d2d9c3
William Jenkins <wfjenkins@ucsd.edu>
2024-05-18 21:11:34 -0700

    Update full run script

--- projects/swellex96_inv/scripts/full_run.sh
+++ projects/swellex96_inv/scripts/full_run.sh
@@ -1,16 +1,18 @@
 # BOGP - UCB
-python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200 --serial=sim_ucb --simulate
-python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=200 --serial=exp_ucb
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=32 --serial=sim_ucb --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=32 --serial=exp_ucb
 
 # BOGP - EI
-python projects/swellex96_inv/data/bo/run.py --optim=ei --init=200 --serial=sim_ei --simulate
-python projects/swellex96_inv/data/bo/run.py --optim=ei --init=200 --serial=exp_ei
-python projects/swellex96_inv/data/bo/run.py --optim=ei --init=400 --serial=exp_ei
-python projects/swellex96_inv/data/bo/run.py --optim=ei --init=300 --serial=exp_ei
-python projects/swellex96_inv/data/bo/run.py --optim=ei --init=100 --serial=exp_ei
-# Only one not apportioned:
-python projects/swellex96_inv/data/bo/run.py --optim=ei --init=50 --serial=exp_ei
+python projects/swellex96_inv/data/bo/run.py --optim=ei --init=32 --serial=sim_ei --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=ei --init=32 --serial=exp_ei
+# python projects/swellex96_inv/data/bo/run.py --optim=ei --init=64 --serial=exp_ei
+# python projects/swellex96_inv/data/bo/run.py --optim=ei --init=16 --serial=exp_ei
+# python projects/swellex96_inv/data/bo/run.py --optim=ei --init=8 --serial=exp_ei
+
+# BOGP - LogEI
+python projects/swellex96_inv/data/bo/run.py --optim=logei --init=32 --serial=sim_logei --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=logei --init=32 --serial=exp_logei
 
 # SOBOL
-# python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=exp_sobol
-# python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=sim_sobol --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=sim_sobol --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=exp_sobol
