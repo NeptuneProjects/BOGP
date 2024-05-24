000329ae09de1a6f5a5c87a9bd87d075ff8a8412
William Jenkins <wfjenkins@ucsd.edu>
2024-05-23 22:02:26 -0700

    Update for new configs

--- projects/swellex96_inv/visualization/warmup_perf.py
+++ projects/swellex96_inv/visualization/warmup_perf.py
@@ -18,8 +18,7 @@ plt.style.use(["science", "ieee"])
 
 # MODES = "Simulated"
 MODES = "Experimental"
-N_INIT = 200
-XLIM = (0, 3000)
+N_TRIALS = 100
 if MODES == "Simulated":
     YLIM = (1e-8, 1.0)
     YTICKS = [1e-8, 1e-6, 1e-4, 1e-2, 1.0]
@@ -28,7 +27,7 @@ if MODES == "Experimental":
     YTICKS = [0.05, 0.1, 0.5, 1.0]
 
 
-def plot_perf_vs_warmup(df: pd.DataFrame, ax: Optional[plt.Axes] = None):
+def plot_perf_vs_warmup(df: pd.DataFrame) -> plt.Figure:
     fig, ax1 = plt.subplots(figsize=(3.0, 1.5))
 
     sns.lineplot(
@@ -38,12 +37,12 @@ def plot_perf_vs_warmup(df: pd.DataFrame, ax: Optional[plt.Axes] = None):
         ax=ax1,
         err_style="bars",
         errorbar=lambda x: (x.min(), x.max()),
-        err_kws={"capsize": 3.0},
+        err_kws={"capsize": 4.0},
         color="tab:blue",
     )
     ax1.set_ylabel("$\hat{\phi}(\mathbf{m})$", color="tab:blue")
     ax1.tick_params(axis="y", labelcolor="tab:blue")
-    ax1.set_xticks([50, 100, 200, 300, 400])
+    ax1.set_xticks([8, 16, 32, 64])
     ax1.tick_params(axis="x", which="minor", bottom=False, top=False)
     ax1.set_xlabel("$N_\mathrm{init}$")
 
@@ -55,7 +54,7 @@ def plot_perf_vs_warmup(df: pd.DataFrame, ax: Optional[plt.Axes] = None):
         ax=ax2,
         err_style="bars",
         errorbar=lambda x: (x.min(), x.max()),
-        err_kws={"capsize": 3.0},
+        err_kws={"capsize": 2.0},
         color="tab:red",
     )
     ax2.set_ylabel("Wall time [s]", color="tab:red")
@@ -67,9 +66,9 @@ def plot_perf_vs_warmup(df: pd.DataFrame, ax: Optional[plt.Axes] = None):
 def main():
     path = common.SWELLEX96Paths.outputs / "runs"
     data = helpers.load_data(
-        path, "exp_ei/*.npz", common.SEARCH_SPACE, common.TRUE_VALUES
+        path, "exp_logei/*.npz", common.SEARCH_SPACE, common.TRUE_EXP_VALUES
     )
-    data = data.loc[data["Trial"] == 500]
+    data = data.loc[data["Trial"] == N_TRIALS]
     return plot_perf_vs_warmup(data)
 
 
