0f0dcbbd401f2d2a5280dc1a3d0a3523807e3bfd
William Jenkins <wfjenkins@ucsd.edu>
2024-05-23 22:02:46 -0700

    Update run script

--- projects/swellex96_inv/scripts/full_run.sh
+++ projects/swellex96_inv/scripts/full_run.sh
@@ -1,17 +1,17 @@
 # BOGP - UCB
-# python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=32 --serial=sim_ucb --simulate
-# python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=32 --serial=exp_ucb
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=64 --serial=sim_ucb --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=ucb --init=64 --serial=exp_ucb
 
 # BOGP - EI
-# python projects/swellex96_inv/data/bo/run.py --optim=ei --init=32 --serial=sim_ei --simulate
-# python projects/swellex96_inv/data/bo/run.py --optim=ei --init=32 --serial=exp_ei
+python projects/swellex96_inv/data/bo/run.py --optim=ei --init=64 --serial=sim_ei --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=ei --init=64 --serial=exp_ei
 
 # BOGP - LogEI
-# python projects/swellex96_inv/data/bo/run.py --optim=logei --init=32 --serial=sim_logei --simulate
+python projects/swellex96_inv/data/bo/run.py --optim=logei --init=64 --serial=sim_logei --simulate
 # python projects/swellex96_inv/data/bo/run.py --optim=logei --init=32 --serial=exp_logei
-python projects/swellex96_inv/data/bo/run.py --optim=logei --init=64 --serial=exp_logei
-python projects/swellex96_inv/data/bo/run.py --optim=logei --init=16 --serial=exp_logei
-python projects/swellex96_inv/data/bo/run.py --optim=logei --init=8 --serial=exp_logei
+# python projects/swellex96_inv/data/bo/run.py --optim=logei --init=64 --serial=exp_logei
+# python projects/swellex96_inv/data/bo/run.py --optim=logei --init=16 --serial=exp_logei
+# python projects/swellex96_inv/data/bo/run.py --optim=logei --init=8 --serial=exp_logei
 
 # SOBOL
 # python projects/swellex96_inv/data/bo/run.py --optim=sobol --budget=50000 --serial=sim_sobol --simulate
