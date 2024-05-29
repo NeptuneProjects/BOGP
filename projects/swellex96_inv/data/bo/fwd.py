639b5703523de49af8c105c662864b8b2b946370
William Jenkins <wfjenkins@ucsd.edu>
2024-05-28 17:39:00 -0700

    Add data check

--- projects/swellex96_inv/data/bo/fwd.py
+++ projects/swellex96_inv/data/bo/fwd.py
@@ -9,12 +9,23 @@ import common
 from obj import get_objective
 
 
+params = {
+    "rec_r": 1.07,
+    "src_z": 72.04,
+    "tilt": 2.14,
+    "h_w": 217.71,
+    "h_s": 20.55,
+    "c_p_sed_top": 1573.32,
+    "dc_p_sed": 16.96,
+}
+
+
 def main(simulate: bool = False) -> None:
     objective = get_objective(simulate=simulate)
     if simulate:
-        return objective(common.TRUE_SIM_VALUES)
+        return objective(params)
     return objective(common.TRUE_EXP_VALUES)
 
 
 if __name__ == "__main__":
-    main()
+    print(main(simulate=True))
