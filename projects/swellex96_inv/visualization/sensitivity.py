b35a4a3528247283bb2af4bf2f8746ceb36f833e
William Jenkins <wfjenkins@ucsd.edu>
2024-05-30 20:02:50 -0700

    Update sens plots for new bounds, split fig

--- projects/swellex96_inv/visualization/sensitivity.py
+++ projects/swellex96_inv/visualization/sensitivity.py
@@ -3,7 +3,6 @@
 
 from pathlib import Path
 import sys
-from typing import Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,84 +15,96 @@ plt.style.use(["science", "ieee"])
 
 
 def plot_sensitivity(
-    sensitivities: np.ndarray,
-    parameters: dict,
-    title: Optional[str] = None,
-    subfiglabel: Optional[str] = None,
+    data: list[np.ndarray],
+    parameters: list[dict],
 ):
-    ncols = 3
-    nrows = (
-        len(sensitivities) // ncols
-        if len(sensitivities) % ncols == 0
-        else len(sensitivities) // ncols + 1
-    )
+    nrows = 7
 
     fig, axs = plt.subplots(
         nrows=nrows,
-        ncols=ncols,
-        figsize=(3, 4),
-        gridspec_kw={"hspace": 0.6, "wspace": 0.25},
+        ncols=1,
+        figsize=(3, 6),
+        gridspec_kw={"hspace": 0.9, "wspace": 0.25},
     )
-    for i, ax in enumerate(axs.reshape(-1)):
-        if i >= len(sensitivities):
-            ax.axis("off")
-            continue
-        if i % 3 != 0:
-            ax.set_yticklabels([])
-        if i == 0:
-            ax.set_ylabel("$\phi(\mathbf{m})$ [dB]")
+    print(axs.shape)
+    for i, ax in enumerate(axs):
 
-        parameter = sensitivities[i]
-        # if parameter["name"] == "dc_p_sed":
-        if "dc" in parameter["name"]:
-            B = parameter["value"] + sensitivities[i - 1]["value"]
-            xlim = [
-                min(parameter["space"] + sensitivities[i - 1]["value"]),
-                max(parameter["space"] + sensitivities[i - 1]["value"]),
-            ]
-        else:
-            B = parameter["value"]
-            xlim = [min(parameter["space"]), max(parameter["space"])]
-        B = 1 - B
-        B = 10 * np.log10(B / B.max())
+        parameter = data[0][i]
 
-        ax.plot(parameter["space"], B, label=parameter["name"])
-        ax.axvline(parameters[parameter["name"]], color="k", linestyle="--")
+        B = parameter["value"]
+        xlim = [min(parameter["space"]), max(parameter["space"])]
+
+        ax.plot(parameter["space"], B, color="tab:blue", label="Simulated")
+        ax.axvline(parameters[0][parameter["name"]], color="tab:blue", linestyle="-")
         ax.set_xlim(xlim)
 
-        if i in [0, 1, 2]:
-            ax.set_ylim([-9.0, 0.5])
-            ax.set_yticks([-9, -6, -3, 0])
-            ax.set_yticks(np.linspace(-9, 0, 4), minor=True)
-        else:
-            ax.set_ylim([-1.0, 0.1])
-            ax.set_yticks([-1, -0.5, 0])
-            ax.set_yticks(np.linspace(-1, 0, 3), minor=True)
+        ax.tick_params(axis="y", labelcolor="tab:blue")
+
+        ax.ticklabel_format(axis="y", style="sci", scilimits=(-6, 5))
         ax.set_xlabel(common.VARIABLES[parameter["name"]], labelpad=0)
 
-    if subfiglabel:
-        fig.text(
-            0, 0.93, subfiglabel, va="top", ha="left", fontsize=10, fontweight="bold"
+        parameter = data[1][i]
+        B = parameter["value"]
+
+        ax2 = ax.twinx()
+        ax2.plot(
+            parameter["space"], B, color="tab:red", linestyle="--", label="Experimental"
         )
-    if title:
-        fig.suptitle(title, y=0.93)
+        ax2.axvline(parameters[1][parameter["name"]], color="tab:red", linestyle="--")
+        ax2.tick_params(axis="y", labelcolor="tab:red")
+
+        if i == 0:
+            ax.set_title("$\phi(\mathbf{m})$", y=1.1)
+
+        if i < 3:
+            ax.set_ylim(0.0, 1.0)
+            ax2.set_ylim(0.0, 1.0)
+        elif i == 3:
+            ax.set_ylim(0.0, 0.08)
+            ax.set_yticks([0.0, 0.08])
+            ax2.set_ylim(0.0, 0.3)
+            ax2.set_yticks([0.0, 0.3])
+        elif i == 4:
+            ax.set_ylim(0.0, 0.01)
+            ax.set_yticks([0.0, 0.01])
+            ax2.set_ylim(0.07, 0.12)
+            ax2.set_yticks([0.07, 0.12])
+        elif i == 5:
+            ax.set_ylim(0.0, 0.00004)
+            ax.set_yticks([0.0, 0.00004])
+            ax.yaxis.set_ticklabels(["0.0", "$4\\times 10^{-5}$"])
+            ax2.set_ylim(0.076, 0.08)
+            ax2.set_yticks([0.076, 0.08])
+        else:
+            ax.set_ylim(0.0, 0.0008)
+            ax.set_yticks([0.0, 0.0008])
+            ax.yaxis.set_ticklabels(["0.0", "$8\\times 10^{-4}$"])
+            ax2.set_ylim(0.076, 0.082)
+            ax2.set_yticks([0.076, 0.082])
+
+    lines1, labels1 = ax.get_legend_handles_labels()
+    fig.legend(lines1, labels1, loc="upper center", ncol=1, bbox_to_anchor=(0.2, 0.925))
+
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    fig.legend(lines2, labels2, loc="upper center", ncol=1, bbox_to_anchor=(0.8, 0.925))
 
     return fig
 
 
 def main():
-    sensitivities = np.load(
+    sens_sim = np.load(
         common.SWELLEX96Paths.outputs / "sensitivity_sim.npy", allow_pickle=True
     )
-    fig = plot_sensitivity(sensitivities, parameters=common.TRUE_SIM_VALUES, title="Simulated data", subfiglabel="(b)")
-    fig.savefig("sensitivity_sim.png")
-
-    sensitivities = np.load(
+    sens_exp = np.load(
         common.SWELLEX96Paths.outputs / "sensitivity_exp.npy", allow_pickle=True
     )
-    fig = plot_sensitivity(sensitivities, parameters=common.TRUE_EXP_VALUES, title="Experimental data", subfiglabel="(c)")
-    fig.savefig("sensitivity_exp.png")
+    fig = plot_sensitivity(
+        data=[sens_sim, sens_exp],
+        parameters=[common.TRUE_SIM_VALUES, common.TRUE_EXP_VALUES],
+    )
+    return fig
 
 
 if __name__ == "__main__":
-    main()
+    fig = main()
+    plt.show()
