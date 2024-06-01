9882fee84feefe3403d34d35f69403ffefb75957
William Jenkins <wfjenkins@ucsd.edu>
2024-06-01 10:34:36 -0700

    Consolidate run scripts

--- projects/swellex96_inv/scripts/run_gpbaxus.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=baxus --init=50 --budget=500 --ndummy=100
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=baxus --init=100 --budget=500 --ndummy=100
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=baxus --init=200 --budget=500 --ndummy=100
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=baxus --init=300 --budget=500 --ndummy=100
-CUDA_VISIBLE_DEVICES=0 python projects/swellex96_inv/data/bo/run.py --optim=baxus --init=400 --budget=500 --ndummy=100
