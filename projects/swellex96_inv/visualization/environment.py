9fe0c1928ad2ab4e969c43f8767b070d9162a3aa
William Jenkins <wfjenkins@ucsd.edu>
2024-05-31 17:16:34 -0700

    Rm unused

--- projects/swellex96_inv/visualization/environment.py
+++ projects/swellex96_inv/visualization/environment.py
@@ -67,72 +67,6 @@ def plot_environment(subfiglabel: str = None):
     # Water SSP
     ax.plot(waterdata["c_p"], waterdata["z"])
 
-    # c1 = 1522.0
-    # c2 = c1
-    # c3 = 1490.0
-    # c4 = c3
-    # c = [c1, c2, c3, c4]
-    # z1 = 2.0
-    # z2 = 5.0
-    # z3 = 30.0
-    # z4 = 215.0
-    # z = [z1, z2, z3, z4]
-
-    # ax.plot(c, z, "ko--", linewidth=0.5, markersize=3)
-    # ax.annotate("$c_1$", xy=(c1, 2), xytext=(1540, 10), arrowprops=dict(arrowstyle="->"), fontsize=7)
-    # ax.annotate("$c_2$", xy=(c2, z2), xytext=(1540, 20), arrowprops=dict(arrowstyle="->"), fontsize=7)
-    # ax.text(1480, z3, "$c_3$", fontsize=7)
-    # ax.text(1480, z4 - 1, "$c_4$", fontsize=7)
-
-    # c_label = 1478.5
-    # # 0 m
-    # z_ = 0
-    # c_ = waterdata["c_p"][0]
-    # ax.scatter(c_, 2, marker="o ", color="k", s=10)
-    # ax.annotate("$c_1$", xy=(c_, 2), xytext=(c_label, z_ + 10), arrowprops=dict(arrowstyle="->"))
-
-    # # 20 m
-    # z_ = 20
-    # ind = np.argmin(np.abs(np.array(waterdata["z"]) - z_))
-    # c_ = waterdata["c_p"][ind]
-    # ax.scatter(c_, z_, marker="o", color="k", s=10)
-    # ax.annotate("$c_2$", xy=(c_, z_), xytext=(c_label, z_), arrowprops=dict(arrowstyle="->"))
-
-    # # 40 m
-    # z_ = 40
-    # ind = np.argmin(np.abs(np.array(waterdata["z"]) - z_))
-    # c_ = waterdata["c_p"][ind]
-    # ax.scatter(c_, z_, marker="o", color="k", s=10)
-    # ax.text(c_label, z_, "$c_3$")
-
-    # # 60 m
-    # z_ = 60
-    # ind = np.argmin(np.abs(np.array(waterdata["z"]) - z_))
-    # c_ = waterdata["c_p"][ind]
-    # ax.scatter(c_, z_, marker="o", color="k", s=10)
-    # ax.text(c_label, z_, "$c_4$")
-
-    # # 80 m
-    # z_ = 80
-    # ind = np.argmin(np.abs(np.array(waterdata["z"]) - z_))
-    # c_ = waterdata["c_p"][ind]
-    # ax.scatter(c_, z_, marker="o", color="k", s=10)
-    # ax.text(c_label, z_, "$c_5$")
-
-    # # 100 m
-    # z_ = 100
-    # ind = np.argmin(np.abs(np.array(waterdata["z"]) - z_))
-    # c_ = waterdata["c_p"][ind]
-    # ax.scatter(c_, z_, marker="o", color="k", s=10)
-    # ax.text(c_label, z_, "$c_6$")
-
-    # # 217 m
-    # z_ = 215
-    # ind = np.argmin(np.abs(np.array(waterdata["z"]) - z_))
-    # c_ = waterdata["c_p"][ind]
-    # ax.scatter(c_, z_, marker="o", color="k", s=10)
-    # ax.text(c_label, z_ - 2, "$c_7$")
-
     # Sediment 1
     ax.fill_between(
         [xlim[0], xlim[1]],
@@ -197,7 +131,7 @@ def plot_environment(subfiglabel: str = None):
     # Source
     ax.scatter(SRC_XLOC, src_z, marker="o", color="k", s=10)
     ax.text(SRC_XLOC, src_z - 10, "Source", ha="center")
-    
+
     # Range Arrow
     arr = mpatches.FancyArrowPatch(
         (SRC_XLOC, 70),
